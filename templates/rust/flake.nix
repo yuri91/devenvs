8ed014aa5e23ca1a5ce8ca9a092cc464d4e7cee7
Yuri Iozzelli <y.iozzelli@gmail.com>
2024-09-16 21:47:35 +0200

    add envrc and git-hooks to rust template

--- templates/rust/flake.nix
+++ templates/rust/flake.nix
@@ -1,10 +1,13 @@
 {
   inputs = {
+    git-hooks.url = "github:cachix/git-hooks.nix";
+    git-hooks.inputs.nixpkgs.follows = "nixpkgs";
+
     rust-overlay.url = "github:oxalica/rust-overlay";
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, ... } @ inputs:
+  outputs = { self, nixpkgs, rust-overlay, git-hooks, ... } @ inputs:
   let
     system = "x86_64-linux";
     pkgs = import nixpkgs {
@@ -18,6 +21,22 @@
     rustPlatform = pkgs.makeRustPlatform {
       rustc = rust-build;
       cargo = rust-build;
+    };
+    pre-commit-check = git-hooks.lib.${system}.run {
+      src = ./.;
+      hooks = {
+        nixpkgs-fmt.enable = true;
+        rustfmt = {
+          enable = true;
+          packageOverrides.cargo = rust-build;
+          packageOverrides.rustfmt = rust-build;
+        };
+        clippy = {
+          enable = true;
+          packageOverrides.cargo = rust-build;
+          packageOverrides.clippy = rust-build;
+        };
+      };
     };
     mycrate = pkgs.callPackage ./package.nix {
       inherit rustPlatform;
@@ -36,6 +55,10 @@
       ];
       RUST_SRC_PATH = "${rust-build}/lib/rustlib/src/rust/library";
       LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath mycrate.runtime-deps}";
+
+      hardeningDisable = [ "all" ];
+
+      inherit (pre-commit-check) shellHook;
     };
     packages.${system}.default = mycrate;
   };
